646b830c041fd91eaec3100320f564593a194dcc
Arielly Andrade <arielly.info@gmail.com>
2026-03-26 21:40:35 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,25 @@
+# estrutura de dados 
+lista = [] # mutavel 
+l = list(range(1,200))
+
+variavel = 'a'
+tupla = () # imutavel 
+t = tuple(range(1, 200))
+
+dicionario = {
+    'nome' : 'lucas',
+    'idade' : 10,
+    'e-mail' : 'lucas@gmail.com'
+    'lista' = [1,2,30]
+ }
+
+# estrutura de fluxo de controle 
+
+# funções 
+print()
+input()
+len()
+sum()
+
+# paradigma 
+# estrutural 
